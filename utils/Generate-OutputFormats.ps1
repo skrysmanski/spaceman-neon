635cf11bdb073a2991759b822c435f430462bffb
Sebastian Krysmanski <noreply@manski.net>
2019-10-05 16:47:48 +0200

    Added script to generate theme for Windows Terminal

---
+++ utils/Generate-OutputFormats.ps1
@@ -0,0 +1,46 @@
+#!/usr/bin/env pwsh
+
+$script:ErrorActionPreference = 'Stop'
+
+function Get-ThemeColors($ColorsFile) {
+    return Get-Content $ColorsFile -Encoding utf8 | ConvertFrom-Json
+}
+
+function Write-WindowsTerminalTheme($ThemeColors, [String] $OutputFile) {
+    $windowsTerminalTheme = @"
+{
+    // From: https://github.com/skrysmanski/spaceman-neon
+    "name" : "Spaceman Neon",
+
+    "background" : "$($ThemeColors.Black)",
+    "foreground" : "$($ThemeColors.Gray)",
+
+    "black" : "$($ThemeColors.Black)",
+    "brightBlack" : "$($ThemeColors.DarkGray)",
+    "blue" : "$($ThemeColors.DarkBlue)",
+    "brightBlue" : "$($ThemeColors.Blue)",
+    "cyan" : "$($ThemeColors.DarkCyan)",
+    "brightCyan" : "$($ThemeColors.Cyan)",
+    "green" : "$($ThemeColors.DarkGreen)",
+    "brightGreen" : "$($ThemeColors.Green)",
+    "purple" : "$($ThemeColors.DarkMagenta)",
+    "brightPurple" : "$($ThemeColors.Magenta)",
+    "red" : "$($ThemeColors.DarkRed)",
+    "brightRed" : "$($ThemeColors.Red)",
+    "yellow" : "$($ThemeColors.DarkYellow)",
+    "brightYellow" : "$($ThemeColors.Yellow)",
+    "white" : "$($ThemeColors.Gray)",
+    "brightWhite" : "$($ThemeColors.White)"
+}
+
+"@
+
+    # NOTE: -NewLine prevent Out-File from appending a system-dependent newline at the end of the file.
+    $windowsTerminalTheme | Out-File $OutputFile -Encoding utf8 -NoNewline
+}
+
+$baseDir = "$PSScriptRoot/.."
+
+$themeColors = Get-ThemeColors "$baseDir/colors.json"
+
+Write-WindowsTerminalTheme $themeColors "$baseDir/WindowsTerminal.json"
