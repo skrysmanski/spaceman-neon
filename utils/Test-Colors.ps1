903ef2ef6c7e505791fea063e5599356a6b753fe
Sebastian Krysmanski <noreply@manski.net>
2019-10-05 16:05:17 +0200

    Added printout for potentially problematic color combos to Test-Colors.ps1

--- utils/Test-Colors.ps1
+++ utils/Test-Colors.ps1
@@ -7,29 +7,61 @@
 # "red / bright red". But in the end they mean the same colors.
 #
 
-function Write-Color([System.ConsoleColor] $color) {
+function Write-Color([System.ConsoleColor] $Color, [switch] $NoNewLine) {
+    # Output color in its color
+    Write-Host -ForegroundColor $Color -NoNewline:$NoNewLine "$Color"
+}
+
+function Write-ColorAndLabel([System.ConsoleColor] $Color) {
     # Label each color for when it's not readable (e.g. black on black background)
-    $alignedLabel = '{0,-12} : ' -f $color
+    $alignedLabel = '{0,-12} : ' -f $Color
     Write-Host -ForegroundColor White -NoNewline $alignedLabel
 
     # Output color in its color
-    Write-Host -ForegroundColor $color "$color"
+    Write-Color $Color
 }
 
+#
+# Print colors
+#
 Write-Host 'Default'
-Write-Color Black
-Write-Color DarkRed
-Write-Color Red
-Write-Color DarkGreen
-Write-Color Green
-Write-Color DarkYellow
-Write-Color Yellow
-Write-Color DarkBlue
-Write-Color Blue
-Write-Color DarkMagenta
-Write-Color Magenta
-Write-Color DarkCyan
-Write-Color Cyan
-Write-Color DarkGray
-Write-Color Gray
-Write-Color White
+Write-ColorAndLabel Black
+Write-ColorAndLabel DarkRed
+Write-ColorAndLabel Red
+Write-ColorAndLabel DarkGreen
+Write-ColorAndLabel Green
+Write-ColorAndLabel DarkYellow
+Write-ColorAndLabel Yellow
+Write-ColorAndLabel DarkBlue
+Write-ColorAndLabel Blue
+Write-ColorAndLabel DarkMagenta
+Write-ColorAndLabel Magenta
+Write-ColorAndLabel DarkCyan
+Write-ColorAndLabel Cyan
+Write-ColorAndLabel DarkGray
+Write-ColorAndLabel Gray
+Write-ColorAndLabel White
+
+#
+# Print potentially problematic color combos. Let's us view the "contrast"
+# differences between the individual colors.
+#
+Write-Host
+
+Write-Color Blue -NoNewLine
+Write-Color Cyan -NoNewLine
+Write-Color Gray -NoNewLine
+Write-Host
+Write-Host
+
+Write-Color Cyan -NoNewLine
+Write-Color Gray -NoNewLine
+Write-Color White -NoNewLine
+Write-Host
+Write-Host
+
+Write-Color DarkRed -NoNewLine
+Write-Color Magenta -NoNewLine
+Write-Color Red -NoNewLine
+Write-Host
+Write-Host
