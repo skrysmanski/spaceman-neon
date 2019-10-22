a2abc9eea9f9b0f7d97a2183666f76c97a9f89b8
Sebastian Krysmanski <noreply@manski.net>
2019-10-22 17:32:07 +0200

    Added support for vscode terminal theme

--- utils/Generate-OutputFormats.ps1
+++ utils/Generate-OutputFormats.ps1
@@ -269,6 +269,41 @@ $(Get-ColorEntry 'Link Color' $ThemeColors.Blue)
     $output.Replace('    ', "`t") | Out-File $OutputFile -Encoding ascii -NoNewline
 }
 
+function Write-VSCodeTerminalTheme($ThemeColors, [String] $OutputFile) {
+    # NOTE: We use a string here rather than an object (in conjunction with ConvertTo-Json) because
+    #   with an object we can't control the order of the properties (neither can we add comments)
+    #   to the output.
+    $output = @"
+    // put in your user settings
+    "workbench.colorCustomizations": {
+        "terminal.background":"$($ThemeColors.Black)",
+        "terminal.foreground":"$($ThemeColors.Gray)",
+        "terminalCursor.background":"$($ThemeColors.Gray)",
+        "terminalCursor.foreground":"$($ThemeColors.Gray)",
+        "terminal.ansiBlack":"$($ThemeColors.Black)",
+        "terminal.ansiBlue":"$($ThemeColors.DarkBlue)",
+        "terminal.ansiBrightBlack":"$($ThemeColors.DarkGray)",
+        "terminal.ansiBrightBlue":"$($ThemeColors.Blue)",
+        "terminal.ansiBrightCyan":"$($ThemeColors.Cyan)",
+        "terminal.ansiBrightGreen":"$($ThemeColors.Green)",
+        "terminal.ansiBrightMagenta":"$($ThemeColors.Magenta)",
+        "terminal.ansiBrightRed":"$($ThemeColors.Red)",
+        "terminal.ansiBrightWhite":"$($ThemeColors.White)",
+        "terminal.ansiBrightYellow":"$($ThemeColors.Yellow)",
+        "terminal.ansiCyan":"$($ThemeColors.DarkCyan)",
+        "terminal.ansiGreen":"$($ThemeColors.DarkGreen)",
+        "terminal.ansiMagenta":"$($ThemeColors.DarkMagenta)",
+        "terminal.ansiRed":"$($ThemeColors.DarkRed)",
+        "terminal.ansiWhite":"$($ThemeColors.Gray)",
+        "terminal.ansiYellow":"$($ThemeColors.DarkYellow)"
+    }
+
+"@
+
+    # NOTE: -NewLine prevent Out-File from appending a system-dependent newline at the end of the file.
+    $output | Out-File $OutputFile -Encoding utf8 -NoNewline
+}
+
 $baseDir = "$PSScriptRoot/.."
 
 $themeColors = Get-ThemeColors "$baseDir/colors.json"
@@ -278,3 +313,5 @@ Write-WindowsTerminalTheme $themeColors "$baseDir/WindowsTerminal.json"
 Write-WindowsConsoleRegFile $themeColors "$baseDir/WindowsConsole.reg"
 
 Write-ItermColorsFile $themeColors "$baseDir/SpacemanNeon.itermcolors"
+
+Write-VSCodeTerminalTheme $themeColors "$baseDir/VSCodeTerminal.json"
